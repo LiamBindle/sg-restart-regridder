94057f5ea098020b55e43a7690554f3756a7dae9
Liam Bindle <liambindle@gmail.com>
2019-11-18 17:27:13 -0600

    more work

--- sg/make_pcolormesh.py
+++ sg/make_pcolormesh.py
@@ -1,5 +1,4 @@
 import sys
-
 import yaml
 
 import matplotlib.pyplot as plt
@@ -9,6 +8,7 @@ import pyproj
 import numpy as np
 import xarray as xr
 import pandas as pd
+from tqdm import tqdm
 
 from sg.pipe_operations import *
 from sg.experiment import Experiment
@@ -18,30 +18,40 @@ from sg.framer import *
 
 
 if __name__ == '__main__':
+    working_dir = os.path.dirname(sys.argv[1])
+
     with open(sys.argv[1], 'r') as f:
         yaml_input = yaml.safe_load(f)
 
+    # Make output directory
+    output_dir = os.path.join(working_dir, os.path.dirname(yaml_input['pcolormesh']['output']))
+    output_fname = os.path.basename(yaml_input['pcolormesh']['output'])
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Create Experiment object
     experiment = Experiment(
         directory=yaml_input['experiment']['directory'],
         grid=eval(yaml_input['experiment']['grid'])
     )
 
+    # Get collections to read and variables to keep
     target = yaml_input['pcolormesh']['target']
     supplemental= yaml_input['pcolormesh']['supplemental']
-
-    dates = pd.date_range(**yaml_input['pcolormesh']['date_range'])
-
     if isinstance(supplemental, str):
         supplemental = [supplemental]
-
     collections = list(set([variable.split(':', 1)[0] for variable in [target, *supplemental]]))
     variables = list(set([variable.split(':', 1)[1] for variable in [target, *supplemental]]))
     target = target.split(':', 1)[1]
 
+    # General settings
     vmin = yaml_input['pcolormesh'].get('vmin', 0)
     vmax = yaml_input['pcolormesh'].get('vmax', None)
 
-    for timestamp in dates:
+    # Loop over times
+    dates = pd.date_range(**yaml_input['pcolormesh']['date_range'])
+    for timestamp in tqdm(dates):
+        # Load data
         ds = experiment.load(
             collections=collections,
             date=timestamp.to_pydatetime(),
@@ -49,18 +59,19 @@ if __name__ == '__main__':
         )
         da = ds[target]
 
-        if vmax is None:
-            vmax = np.asscalar(da.max().values)
-
+        # Perform operation
         callable_operation = eval(yaml_input['pcolormesh']['operation'])
         da = callable_operation(da, ds, experiment.key_lut)
 
+        # General figure settings
+        if vmax is None:
+            vmax = np.asscalar(da.max().values)
 
-
-
+        # Create figure
         plt.figure(figsize=(16, 8), dpi=100)
         figax = plate_carree(experiment)
 
+        # Loop over faces
         for face in range(6):
             xx, yy = figax.transform_xy(experiment.grid.xe(face), experiment.grid.ye(face))
 
@@ -68,7 +79,7 @@ if __name__ == '__main__':
 
             # Draw grid
             draw_minor_grid_boxes(figax, xx, yy)
-            draw_major_grid_boxes(figax, xx, yy)
+            #draw_major_grid_boxes(figax, xx, yy)
 
             # Plot data
             pcolormesh = plot_pcolomesh(figax, xx, yy, face_data, vmin=vmin, vmax=vmax)
@@ -78,9 +89,10 @@ if __name__ == '__main__':
                 cb.set_label(yaml_input['pcolormesh'].get('units', ''))
 
             # Draw face number
-            text = draw_face_number(figax, xx, yy, face)
-            draw_text_stroke(text)
+            # text = draw_face_number(figax, xx, yy, face)
+            # draw_text_stroke(text)
         plt.title(yaml_input['pcolormesh'].get('title', ''))
         plt.tight_layout()
-        plt.show()
+        plt.savefig(os.path.join(output_dir, output_fname.format(timestamp=timestamp)))
+        plt.close()
 
