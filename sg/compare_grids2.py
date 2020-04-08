610d9c67ad9f464fa2803887b2a2be6cb02555a2
Liam Bindle <liambindle@gmail.com>
2020-04-07 23:55:23 -0600

    intersect weight average almost works

--- sg/compare_grids2.py
+++ sg/compare_grids2.py
@@ -4,6 +4,7 @@ import shapely.wkb
 import cartopy.crs as ccrs
 import scipy.spatial
 import pyproj
+import shapely.strtree
 
 import sg.grids
 
@@ -22,7 +23,7 @@ def get_major_xy(xe, ye):
     return np.moveaxis(np.array([boxes_x, boxes_y]), 0, -1)
 
 
-def transform_xy(in_proj: pyproj.Proj, out_proj: pyproj.Proj, xy_in):
+def transform_xy(xy_in, in_proj: pyproj.Proj, out_proj: pyproj.Proj):
     xy_out = pyproj.transform(in_proj, out_proj, xy_in[..., 0], xy_in[..., 1])
     xy_out = np.moveaxis(xy_out, 0, -1)
     return xy_out
@@ -39,7 +40,7 @@ def polygon_areas(polygons):
     return areas
 
 
-def xy_to_polygons(xy, transform=ccrs.PlateCarree()):
+def xy_to_polygons(xy, transform=None, error_on_bad_polygon=True):
     if len(xy.shape) == 2:
         xy = np.expand_dims(xy, 0)
 
@@ -56,13 +57,19 @@ def xy_to_polygons(xy, transform=ccrs.PlateCarree()):
         if polygons[i].area <= 0:
             zero_area.append(i)
 
-    if len(bad) > 0 or len(zero_area) > 0:
+    if error_on_bad_polygon and (len(bad) > 0 or len(zero_area) > 0):
+        if transform is not None:
+            ax = quick_map(projection=transform)
+            draw_polygons(ax, xy[bad], transform, color='red')
+            plt.show()
         raise RuntimeError('A bad polygon was detected')
+    elif len(bad) > 0 or len(zero_area) > 0:
+        for bad_index in [*bad, *zero_area]:
+            polygons[bad_index] = shapely.geometry.Polygon([(0,0), (0,0), (0,0)]) # zero area
     return np.reshape(polygons, output_shape)
 
-import shapely.strtree
 
-def p1_intersects_in_p2(p1: np.ndarray, p2: np.ndarray):
+def p1_intersects_in_p2_extent(p1: np.ndarray, p2: np.ndarray, return_slices=True):
     p1_indexes = np.array([ar.flatten() for ar in np.meshgrid(*[np.arange(s) for s in p1.shape], indexing='ij')])
     p1 = p1.flatten().tolist()
     index_by_id = dict((id(poly), i) for i, poly in enumerate(p1))
@@ -75,151 +82,75 @@ def p1_intersects_in_p2(p1: np.ndarray, p2: np.ndarray):
 
     for i, poly in enumerate(p2.flatten()):
         indexes = [[dim[i] for dim in p1_indexes] for i in query_one_polygon(poly)]
-        indexes = np.moveaxis(indexes, 0, -1)
-        indexes = tuple(indexes)
+        if return_slices:
+            indexes = np.moveaxis(indexes, 0, -1)
+            indexes = tuple(indexes)
         p1_intersects[i] = indexes
 
     return p1_intersects.reshape(p2.shape)
 
+import scipy.sparse
+from tqdm import tqdm
+
+def ciwam(grid_in: sg.grids.CSDataBase, grid_out: sg.grids.CSDataBase):
+    # rows correspond to boxes in grid_out, columns correspond to boxes in grid_in
+    latlon = pyproj.Proj('+init=epsg:4326')
+
+    M_data = []
+    M_i = []
+    M_j = []
+
+    flat_index = lambda grid, f, i, j: f*(grid.csres**2) + i*grid.csres + j
+
+    for face_out in tqdm(range(6), desc='Output face', unit='face'):
+        minor_out_ll = get_minor_xy(grid_out.xe(face_out) % 360, grid_out.ye(face_out))
+
+        center_x = (grid_out.xe(face_out) % 360)[grid_out.csres//2, grid_out.csres//2]
+        center_y = grid_out.ye(face_out)[grid_out.csres // 2, grid_out.csres // 2]
+        laea = pyproj.Proj(
+            f'+proj=laea +lat_0={center_y} +lon_0={center_x}  +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs'
+        )
+
+        minor_out_ea = transform_xy(minor_out_ll, latlon, laea)
+        minor_out = xy_to_polygons(minor_out_ea)
+
+        for face_in in tqdm(range(6), desc='Input face', unit='face'):
+            minor_in_ll = get_minor_xy(grid_in.xe(face_in) % 360, grid_in.ye(face_in))
+            minor_in_ea = transform_xy(minor_in_ll, latlon, laea)
+            minor_in = xy_to_polygons(minor_in_ea, error_on_bad_polygon=False) #transform=ccrs.LambertAzimuthalEqualArea(center_x, center_y))
+
+            minor_in_indexes = p1_intersects_in_p2_extent(minor_in, minor_out, return_slices=False)
+
+            for i in range(grid_out.csres):
+                for j in range(grid_out.csres):
+                    row_index = flat_index(grid_out, face_out, i, j)
+                    for indexes in minor_in_indexes[i, j]:
+                        col_index = flat_index(grid_in, face_in, indexes[0], indexes[1])
+
+                        gridbox_out = minor_out[i, j]
+                        gridbox_in = minor_in[indexes[0], indexes[1]]
+
+                        weight = gridbox_out.intersection(gridbox_in).area / gridbox_out.area
+
+                        if weight > 0:
+                            M_data.append(weight)
+                            M_i.append(row_index)
+                            M_j.append(col_index)
+
+    M = scipy.sparse.coo_matrix((M_data, (M_i, M_j)), shape=(6 * grid_out.csres ** 2, 6 * grid_in.csres ** 2))
+
+    # QA
+    weight_sum = M.sum(axis=1)
+
+    return M
 
 
 
 
 
-def get_am_and_pm_masks_and_polygons_outline(xe, ye, far_from_pm=80):
-    if np.any(xe >= 180):
-        raise ValueError('xe must be in [-180, 180)')
-    # xe must be [-180 to 180]
-    p0 = slice(0, -1)
-    p1 = slice(1, None)
-
-    # Mask where bounding box crosses the prime meridian or antimeridian
-    cross_pm_or_am_line1 = np.not_equal(np.sign(xe[p0, p0]), np.sign(xe[p1, p0]))
-    cross_pm_or_am_line2 = np.not_equal(np.sign(xe[p1, p0]), np.sign(xe[p1, p1]))
-    cross_pm_or_am_line3 = np.not_equal(np.sign(xe[p1, p1]), np.sign(xe[p0, p1]))
-    cross_pm_or_am_line4 = np.not_equal(np.sign(xe[p0, p1]), np.sign(xe[p0, p0]))
-    cross_pm_or_am = cross_pm_or_am_line1 | cross_pm_or_am_line2 | cross_pm_or_am_line3 | cross_pm_or_am_line4
-
-    # Make xy polygons for each gridbox
-    boxes_x = np.moveaxis(np.array([xe[p0, p0], xe[p1, p0], xe[p1, p1], xe[p0, p1]]), 0, -1)
-    boxes_y = np.moveaxis(np.array([ye[p0, p0], ye[p1, p0], ye[p1, p1], ye[p0, p1]]), 0, -1)
-    polygon_outlines = np.moveaxis(np.array([boxes_x, boxes_y]), 0, -1)
-
-    pm = np.ones((xe.shape[0]-1, xe.shape[1]-1), dtype=bool)
-    am = np.ones((xe.shape[0]-1, xe.shape[1]-1), dtype=bool)
-
-    # Figure out which polygon_outlines cross the prime meridian and antimeridian
-    crossing_indexes = np.argwhere(cross_pm_or_am)
-    for idx in crossing_indexes:
-        box = shapely.geometry.LinearRing(polygon_outlines[tuple(idx)])
-        far_from_the_prime_meridian = shapely.geometry.LineString([(far_from_pm, -90), (80, far_from_pm)])
-        if box.crosses(far_from_the_prime_meridian):
-            am[tuple(idx)] = False
-        else:
-            pm[tuple(idx)] = False
-
-    return am, pm, polygon_outlines
 
 
-def colocated_centers(xc1, yc1, xc2, yc2, dist_tol_abs):
-    indexes1 = np.array([ar.flatten() for ar in np.meshgrid(*[np.arange(s) for s in xc1.shape], indexing='ij')])
-    indexes2 = np.array([ar.flatten() for ar in np.meshgrid(*[np.arange(s) for s in xc2.shape], indexing='ij')])
-    data1 = np.moveaxis([xc1.flatten(), yc1.flatten()], 0, -1)
-    data2 = np.moveaxis([xc2.flatten(), yc2.flatten()], 0, -1)
-    kdtree1 = scipy.spatial.KDTree(data1)
-    kdtree2 = scipy.spatial.KDTree(data2)
-    nearby = kdtree1.query_ball_tree(kdtree2, dist_tol_abs)
 
-    # nearby is list of indexes in 2 for each in 1
-    co1 = []
-    co2 = []
-    for i, neighbors in enumerate(nearby):
-        for neighbor in neighbors:
-            co1.append(indexes1[:,i])
-            co2.append(indexes2[:,neighbor])
-
-    # Put into format that can index original arrays
-    co1 = tuple([idx_list for idx_list in np.moveaxis(np.array(co1), 0, -1)])
-    co2 = tuple([idx_list for idx_list in np.moveaxis(np.array(co2), 0, -1)])
-    return co1, co2
-
-import shapely.errors
-
-def comparable_gridboxes(control_grid, exp_grid, dist_tol_abs, area_tol_rel=None, intersect_tol_rel=None, target_lat=None, target_lon=None, inside=None):
-    # Get center coordinates
-    control_xc = np.array([control_grid.xc(i) for i in range(6)])
-    control_yc = np.array([control_grid.yc(i) for i in range(6)])
-    experiment_xc = np.array([exp_grid.xc(i) for i in range(6)])
-    experiment_yc = np.array([exp_grid.yc(i) for i in range(6)])
-
-    # Convert coordinates to equidistant projection with units of meters
-    equidistant_meters = pyproj.Proj(init='epsg:4087')
-    control_xc_m, control_yc_m = equidistant_meters(control_xc, control_yc)
-    experiment_xc_m, experiment_yc_m = equidistant_meters(experiment_xc, experiment_yc)
-
-    # Find colocated centers with a tolerance of 100km
-    co1, co2 = colocated_centers(control_xc_m, control_yc_m, experiment_xc_m, experiment_yc_m,
-                                 dist_tol_abs=dist_tol_abs)
-
-    # Get polygons
-    control_boxes_xy = np.ndarray((6, *control_xc.shape[1:], 4, 2))  # [face, i, i, pts, xy]
-    experiment_boxes_xy = np.ndarray((6, *experiment_xc.shape[1:], 4, 2))  # [face, i, i, pts, xy]
-    for i in range(6):
-        xx = control_grid.xe(i)
-        xx[xx > 180] -= 360
-        _, _, control_boxes_xy[i, ...] = get_am_and_pm_masks_and_polygons_outline(xx, control_grid.ye(i))
-        xx = exp_grid.xe(i)
-        xx[xx > 180] -= 360
-        _, _, experiment_boxes_xy[i, ...] = get_am_and_pm_masks_and_polygons_outline(xx, exp_grid.ye(i))
-
-    # Colocated boxes
-    co1_boxes_xy = control_boxes_xy[co1]
-    co2_boxes_xy = experiment_boxes_xy[co2]
-
-    # Project to equal area coordinate system with units of meters
-    equal_area_meters = pyproj.Proj(f'+proj=laea +lon_0={target_lon} +lat_0={target_lat} +units=m')
-    co1_boxes_x_m, co1_boxes_y_m = equal_area_meters(co1_boxes_xy[..., 0], co1_boxes_xy[..., 1])
-    co1_boxes_xy_m = np.moveaxis([co1_boxes_x_m, co1_boxes_y_m], 0, -1)
-    co2_boxes_x_m, co2_boxes_y_m = equal_area_meters(co2_boxes_xy[..., 0], co2_boxes_xy[..., 1])
-    co2_boxes_xy_m = np.moveaxis([co2_boxes_x_m, co2_boxes_y_m], 0, -1)
-
-    # Convert to polygons
-    co1_boxes = np.array([shapely.geometry.Polygon(outline) for outline in co1_boxes_xy_m])
-    co2_boxes = np.array([shapely.geometry.Polygon(outline) for outline in co2_boxes_xy_m])
-    co1_box_areas = np.array([box.area for box in co1_boxes])
-    co2_box_areas = np.array([box.area for box in co2_boxes])
-    if area_tol_rel is not None:
-        similar_areas = np.abs(co2_box_areas - co1_box_areas) / co1_box_areas < area_tol_rel
-    elif intersect_tol_rel is not None:
-        intersections = []
-        for b1, b2 in zip(co1_boxes, co2_boxes):
-            try:
-                if b1.is_valid and b2.is_valid:
-                    intersect_area = b1.intersection(b2).area
-                else:
-                    intersect_area = 0
-            except shapely.errors.ShapelyError:
-                intersect_area = 0
-            intersections.append(intersect_area)
-        intersections = np.array(intersections)
-        co1_similar = np.abs(intersections - co1_box_areas) / co1_box_areas < intersect_tol_rel
-        co2_similar = np.abs(intersections - co2_box_areas) / co2_box_areas < intersect_tol_rel
-        similar_areas = co1_similar & co2_similar
-
-    # Find boxes inside a polygon
-    if inside:
-        with open(inside, 'rb') as f:
-            polygon = shapely.wkb.load(f).buffer(0.5)
-
-        co2_ll_boxes = np.array([shapely.geometry.Polygon(outline) for outline in co2_boxes_xy])
-        contained = np.array([polygon.contains(box) for box in co2_ll_boxes])
-        similar_areas &= contained
-
-    # Find boxes with similar areas
-    co1_final = tuple([indexes[similar_areas] for indexes in co1])
-    co2_final = tuple([indexes[similar_areas] for indexes in co2])
-
-    return co1_final, co2_final
 
 
 def quick_map(projection=ccrs.PlateCarree(), set_global=True, coastlines=True):
@@ -232,19 +163,14 @@ def quick_map(projection=ccrs.PlateCarree(), set_global=True, coastlines=True):
     return ax
 
 
-def plot_polygon(ax, polygon_xy, transform, **kwargs):
-    assert len(polygon_xy.shape) == 2
-    ax.plot(polygon_xy[:, 0], polygon_xy[:, 1], transform=transform, **kwargs)
-
-
-def plot_polygons(ax, polygons_xy, transform, **kwargs):
+def draw_polygons(ax, polygons_xy, transform, **kwargs):
     if len(polygons_xy.shape) == 2:
         polygons_xy = np.expand_dims(polygons_xy, 0)
 
     stacked = np.product(polygons_xy.shape[:-2])
     polygons_xy = np.reshape(polygons_xy, (stacked, *polygons_xy.shape[-2:]))
     for polygon_xy in polygons_xy:
-        plot_polygon(ax, polygon_xy, transform, **kwargs)
+        ax.plot(polygon_xy[:, 0], polygon_xy[:, 1], transform=transform, **kwargs)
 
 
 if __name__=='__main__':
@@ -264,17 +190,16 @@ if __name__=='__main__':
     control = sg.grids.CubeSphere(24)
     experiment = sg.grids.StretchedGrid(12, sf, target_lat, target_lon)
 
+    ciwam(experiment, control)
+
     minor = get_minor_xy(experiment.xe(5) % 360, experiment.ye(5))
-    major = get_major_xy(experiment.xe(5) % 360, experiment.ye(5))
 
-    major_ctl = get_major_xy(control.xe(4) % 360, control.ye(4))
+    minor_exp_xy = minor
+    minor_exp = xy_to_polygons(minor_exp_xy)
+    minor_ctl_xy = get_minor_xy(control.xe(4) % 360, control.ye(4))
+    minor_ctl = xy_to_polygons(minor_ctl_xy)
 
-
-    p1 = xy_to_polygons(minor)
-    p2 = xy_to_polygons(major_ctl)
-
-
-    p1_intersects = p1_intersects_in_p2(p1, p2)
+    p1_intersects = p1_intersects_in_p2_extent(minor_exp, minor_ctl)
 
 
     latlon = pyproj.Proj('+init=epsg:4326')
@@ -286,33 +211,9 @@ if __name__=='__main__':
 
     gno = pyproj.Proj(gno_ccrs.proj4_init)
 
-    minor_gno = transform_xy(latlon, gno, minor)
+    minor_gno = transform_xy(minor, latlon, gno)
 
     ax = quick_map(projection=ccrs.PlateCarree())
-    plot_polygons(ax, minor[p1_intersects[0]], ccrs.PlateCarree(), color='k')
-    plot_polygons(ax, major_ctl, ccrs.PlateCarree(), color='red')
+    draw_polygons(ax, minor_ctl_xy[22,0], ccrs.PlateCarree(), color='k')
+    draw_polygons(ax, minor_exp_xy[p1_intersects[22,0]], ccrs.PlateCarree(), color='red')
     plt.show()
-
-    # co1, co2, intersect = many_comparable_gridboxes(control, experiment)
-    #
-    #
-    # ax = plt.axes(projection=ccrs.PlateCarree())
-    # ax.set_global()
-    # ax.coastlines(linewidth=0.8)
-    # control_xc = np.array([control.xc(i) for i in range(6)])
-    # control_yc = np.array([control.yc(i) for i in range(6)])
-    # experiment_xc = experiment.xc(5)
-    # experiment_yc = experiment.yc(5)
-    # # experiment_xc = np.array([experiment.xc(i) for i in range(6)])
-    # # experiment_yc = np.array([experiment.yc(i) for i in range(6)])
-    # [draw_minor_grid_boxes(ax, control.xe(i), control.ye(i), color='blue') for i in range(6)]
-    # draw_minor_grid_boxes(ax, experiment.xe(5), experiment.ye(5), linewidth=1)
-    #
-    # for i in co1:
-    #     plt.scatter(control_xc[i], control_yc[i], color='red', s=50)
-    # for i in co2:
-    #     plt.scatter(experiment_xc[i], experiment_yc[i], color='pink', s=50)
-    #
-    # print(len(co2[0]), np.count_nonzero(co2[0]==5))
-    # plt.show()
-
