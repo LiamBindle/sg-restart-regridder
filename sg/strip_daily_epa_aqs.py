f315eab929197c6b25796f9ff4b4ff0e9dd2072f
Liam Bindle <liambindle@gmail.com>
2020-04-24 10:28:42 -0600

    made more general

--- sg/strip_daily_epa_aqs.py
+++ sg/strip_daily_epa_aqs.py
@@ -9,20 +9,15 @@ import sklearn.metrics
 from tqdm import tqdm
 
 
-def load_daily(year=2016, product=42602):
-
-    return aqs
-
 def central_angle(x0, y0, x1, y1):
     RAD2DEG = 180 / np.pi
     DEG2RAD = np.pi / 180
-
     x0 = x0 * DEG2RAD
     x1 = x1 * DEG2RAD
     y0 = y0 * DEG2RAD
     y1 = y1 * DEG2RAD
-
     return np.arccos(np.sin(y0) * np.sin(y1) + np.cos(y0) * np.cos(y1) * np.cos(np.abs(x0-x1))) * RAD2DEG
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -34,9 +29,9 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--var',
                         type=str,
                         default='SpeciesConc_NO2',)
-    parser.add_argument('--min-obs-count',
-                        type=int,
-                        default=24)
+    parser.add_argument('--coverage-thresh',
+                        type=float,
+                        default=100)
     parser.add_argument('--start-date',
                         type=str,
                         default=None)
@@ -47,47 +42,80 @@ if __name__ == '__main__':
                         type=str,
                         default='daily_aqs_comparison.csv')
     args = parser.parse_args()
+
+    # Load AQS table
     aqs = pd.read_csv(args.aqs)
+    # Convert date rows to type datetime
     aqs['Date Local'] = pd.to_datetime(aqs['Date Local'])
     aqs['Date of Last Change'] = pd.to_datetime(aqs['Date of Last Change'])
 
+    # Retain relevant rows
     aqs = aqs.loc[aqs['State Name'] == 'California']                # Only California
-    aqs = aqs.loc[aqs['Observation Count'] >= args.min_obs_count]   # Only full samples
+    aqs = aqs.loc[aqs['Observation Percent'] >= args.coverage_thresh]   # Only full samples
 
+    # Reindex according to dates (for comparison w/ simulation) and the station identifier
     aqs = aqs.set_index(['Date Local', 'State Code', 'County Code', 'Site Num']).sort_index()
 
+    # Declare diagnostic file path templates for 24-hr average
     diag_fpath = [
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_0830z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_0930z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1030z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1130z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1230z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1330z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1430z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1530z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1630z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1730z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1830z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_1930z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_2030z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_2130z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_2230z.nc4',
-        'GCHP.SpeciesConc.{date.year:04d}{date.month:02d}{date.day:02d}_2330z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0030z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0130z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0230z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0330z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0430z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0530z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0630z.nc4',
-        'GCHP.SpeciesConc.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0730z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_0830z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_0930z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1030z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1130z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1230z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1330z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1430z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1530z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1630z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1730z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1830z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_1930z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_2030z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_2130z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_2230z.nc4',
+        'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_2330z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0030z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0130z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0230z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0330z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0430z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0530z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0630z.nc4',
+        'GCHP.{collection}.{next_day.year:04d}{next_day.month:02d}{next_day.day:02d}_0730z.nc4',
     ]
+    if args.var in ['SpeciesConc_NO2', 'SpeciesConc_O3', 'SpeciesConc_SO2', 'SpeciesConc_CO']:
+        collection='SpeciesConc'
+    elif args.var == 'PM25':
+        collection = 'AerosolMass'
 
+
+    # Define scale factors that are applied to simulated concentrations
+    scale_factors = {
+        'SpeciesConc_NO2':  1e9,
+        'SpeciesConc_R4N2': 1e9,
+        'SpeciesConc_PAN':  1e9,
+        'SpeciesConc_HNO3': 1e9,
+    }
+
+    # Cache of station identifier -> simulation grid index
     index_cache = {}
 
+    # Make the new table
     aqs_new = aqs.copy()
     aqs_new['Simulated Mean'] = np.nan
+    if args.var in ['SpeciesConc_NO2']:
+        aqs_new['Corrected Arithmetic Mean'] = np.nan
+        calculate_corrected_NO2 = True
+    else:
+        calculate_corrected_NO2 = False
 
+    # Dataset variables that are kept after being loaded
+    if args.var in ['SpeciesConc_NO2']:
+        keep_vars = ['SpeciesConc_NO2', 'SpeciesConc_R4N2', 'SpeciesConc_PAN', 'SpeciesConc_HNO3']
+    else:
+        keep_vars = [args.var]
+
+    # Loop through dates
     for date, new_df in tqdm(aqs.groupby(level=0), desc='Date'):
         rpaths = [os.path.join(args.datadir, fpath.format(date=date, next_day=date + pd.Timedelta(days=1))) for fpath in diag_fpath]
         rpaths_missing = any([not os.path.exists(rpath) for rpath in rpaths])
@@ -103,12 +131,15 @@ if __name__ == '__main__':
             if date > pd.to_datetime(args.end_date):
                 continue
 
-        da = xr.open_mfdataset(
+        ds = xr.open_mfdataset(
             rpaths,
             combine='nested', concat_dim='time',
             data_vars='minimal', coords='minimal',
             compat='override'
-        ).isel(lev=0).squeeze()['SpeciesConc_NO2'].mean('time') * 1e9
+        ).isel(lev=0).squeeze()
+        drop_vars = [v for v in ds.data_vars if v not in keep_vars]
+        ds = ds.drop(drop_vars)
+        ds = ds.mean('time')
 
         sites = new_df.loc[date]
 
@@ -117,18 +148,24 @@ if __name__ == '__main__':
         for new_site in new_sites:
             site_lon = sites.loc[new_site]['Longitude']
             site_lat = sites.loc[new_site]['Latitude']
-            distances = central_angle(da.lons, da.lats, site_lon, site_lat)
+            distances = central_angle(ds.lons, ds.lats, site_lon, site_lat)
             index = np.unravel_index(distances.argmin(), distances.shape)
             index_cache[new_site] = index
 
         slices = [index_cache[site] for site in sites.index]
         slices = list(zip(*slices))
 
-        mean_values = [float(da[nf, Ydim, Xdim]) for nf, Ydim, Xdim in zip(*slices)]
+        simulated_means = {
+            v: np.array([float(ds[v][nf, Ydim, Xdim]) for nf, Ydim, Xdim in zip(*slices)])*scale_factors[v] for v in keep_vars
+        }
 
-        da.close()
+        aqs_new.at[new_df.index, 'Simulated Mean'] = simulated_means[args.var]
 
-        aqs_new.at[new_df.index, 'Simulated Mean'] = mean_values
+        if calculate_corrected_NO2:
+            corrected = aqs_new.loc[new_df.index]['Arithmetic Mean'] / (simulated_means['SpeciesConc_NO2'] + simulated_means['SpeciesConc_R4N2'] + 0.95 * simulated_means['SpeciesConc_PAN'] + 0.15 * simulated_means['SpeciesConc_HNO3'])
+            aqs_new.at[new_df.index, 'Corrected Arithmetic Mean'] = corrected
+
+        ds.close()
 
     aqs_new = aqs_new.dropna()
 
