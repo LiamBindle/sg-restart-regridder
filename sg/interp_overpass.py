579d00219d5fb048655bd29f4755546dc8e9a3f1
Liam Bindle <liambindle@gmail.com>
2020-05-05 15:33:32 -0600

    foo

--- sg/interp_overpass.py
+++ sg/interp_overpass.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     args = vars(args)
 
     base_date = pd.to_datetime(f"{args['date']}T{args['overpass_time']}")
-    start_date = base_date - pd.DateOffset(hours=12)
-    end_date = base_date + pd.DateOffset(hours=12)
+    start_date = base_date - pd.DateOffset(hours=13)
+    end_date = base_date + pd.DateOffset(hours=13)
 
     unresolved_path = 'GCHP.{collection}.{date.year:04d}{date.month:02d}{date.day:02d}_{date.hour:02d}{date.minute:02d}z.nc4'
     files = [os.path.join(args['datadir'], unresolved_path.format(collection=args['collection'], date=date)) for date in pd.date_range(start_date, end_date, freq='30min')]
@@ -63,10 +63,27 @@ if __name__ == '__main__':
 
     solar_time_timedelta_min = lons/360 * 24 * 60
     solar_time_timedelta_min = solar_time_timedelta_min.astype('timedelta64[m]')
-    solar_time_timedelta_min = solar_time_timedelta_min + base_date.to_datetime64()
-    ds.coords['solar_time'] = (['nf', 'Ydim', 'Xdim'], solar_time_timedelta_min)
+    solar_time = solar_time_timedelta_min + base_date.to_datetime64()
 
-    ds2 = ds.sel(time=ds.solar_time, method='nearest', tolerance=np.timedelta64(15, 'm'))
+    solar_time_floor_timedelta_min = (np.floor(solar_time_timedelta_min.astype(float) / 30) * 30).astype('timedelta64[m]')
+    solar_time_ceil_timedelta_min = (np.ceil(solar_time_timedelta_min.astype(float) / 30) * 30).astype('timedelta64[m]')
+    solar_time_floor = solar_time_floor_timedelta_min + base_date.to_datetime64()
+    solar_time_ceil = solar_time_ceil_timedelta_min + base_date.to_datetime64()
+
+    floor_weight = 1 - (solar_time_timedelta_min.astype(int) % 30) / 30
+    ceil_weight = 1 - floor_weight
+
+    ds.coords['solar_time'] = (['nf', 'Ydim', 'Xdim'], solar_time)
+    ds.coords['solar_time_floor'] = (['nf', 'Ydim', 'Xdim'], solar_time_floor)
+    ds.coords['solar_time_ceil'] = (['nf', 'Ydim', 'Xdim'], solar_time_ceil)
+
+    # ds2 = ds.sel(time=ds.solar_time, method='nearest', tolerance=np.timedelta64(15, 'm'))
+    floor = ds.sel(time=ds.solar_time_floor)
+    ceil = ds.sel(time=ds.solar_time_ceil)
+
+    ds2 = floor * floor_weight + ceil * ceil_weight
+
+    print(ds2)
 
     exit(0)
 
